3480a7023ee5cfcaab6dd00a12c8c3fc65d10979
naofalhakim <hakimnaofal@gmail.com>
2017-12-17 20:08:03 +0700

    pesan parkir done

---
+++ src/main/java/com/mycompany/impal/ControllerPesanLokasi.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.impal;
+
+import com.mycompany.impal.view.PesanLokasi;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author naofal
+ */
+public class ControllerPesanLokasi implements ActionListener {
+
+    private ApplicationModel model;
+    private PesanLokasi view;
+
+    public ControllerPesanLokasi() {
+        model = new ApplicationModel();
+        view = new PesanLokasi();
+        view.setActionListener(this);
+        view.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == view.getjButton1()) {
+            
+            int lantai = Integer.parseInt(view.getCmbLantai().getItemAt(view.getCmbLantai().getSelectedIndex()));
+            int nomor = Integer.parseInt(view.getCmbSektor1().getItemAt(view.getCmbSektor1().getSelectedIndex()));
+            String sector = view.getCmbSektor().getItemAt(view.getCmbSektor().getSelectedIndex());
+            String kategori = null;
+            if (view.getJrmobil().isSelected()) {
+                kategori = view.getJrmobil().getText();
+            } else {
+                kategori = view.getJrmotor().getText();
+            }
+
+            int idLokasi = model.cariLokasi(lantai, nomor, sector, kategori);
+
+            String tanggal = view.getCmbTanggal().getSelectedItem().toString()
+                    + "-" + view.getCmbBulan().getSelectedItem().toString() + "-"
+                    + view.getCmbTahun().getSelectedItem().toString();
+            if (idLokasi != 0) {
+                model.pesanLokasi(idLokasi, tanggal, ApplicationModel.idPengendara);
+            } else {
+                JOptionPane.showMessageDialog(view, "Lokasi Telah di Pesan");
+            }
+        }else if(source == view.getBtnKembali()){
+            new ControllerMenuUtama();
+            view.setVisible(false);
+        }
+    }
+
+}

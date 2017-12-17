d57399f5a9ed5f8c10012ce31ce86a5d62c4ecac
naofalhakim <hakimnaofal@gmail.com>
2017-12-17 11:11:20 +0700

    login register

---
+++ src/main/java/com/mycompany/impal/ControllerMenuUtama.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.impal;
+
+import com.mycompany.impal.view.MenuUtama;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author naofal
+ */
+public class ControllerMenuUtama implements ActionListener{
+    private ApplicationModel model;
+    private MenuUtama view;
+
+    public ControllerMenuUtama() {
+        model = new ApplicationModel();
+        view = new MenuUtama();
+        view.setVisible(true);
+        view.setActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if(source == view.getJPantau()){
+            
+        }else if(source == view.getJPesanLokasi()){
+            
+        }
+    }
+    
+}
